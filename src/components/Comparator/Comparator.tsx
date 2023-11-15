646a2aa0e113b1534b509b5272e0849513d0cb09
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-11-15 12:56:56 -0500

    [add][fix] improving comparator design

--- src/components/Comparator/Comparator.tsx
+++ src/components/Comparator/Comparator.tsx
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Comparator.module.scss';
+import { compareTexts } from '../../utils/compareUtils';
 
 const Comparator = () => {
-    const [inputOne, setInputOne] = useState('');
-    const [inputTwo, setInputTwo] = useState('');
-    const [difference, setDifference] = useState('');
+  const [inputOne, setInputOne] = useState('');
+  const [inputTwo, setInputTwo] = useState('');
+  const [highlightedTextOne, setHighlightedTextOne] = useState('');
+  const [highlightedTextTwo, setHighlightedTextTwo] = useState('');
 
-    const compareTexts = () => {
-        // Lógica de comparación aquí (CompareUtils)
-        setDifference('Resultado de la comparación');
-    };
+  useEffect(() => {
+    const { textOne, textTwo } = compareTexts(inputOne, inputTwo);
+    setHighlightedTextOne(textOne);
+    setHighlightedTextTwo(textTwo);
+  }, [inputOne, inputTwo]);
 
-    return (
-        <div className={styles.comparator}>
-            <div className={styles.boxContainer}>
-                <textarea value={inputOne} onChange={e => setInputOne(e.target.value)} placeholder="Ingresa el primer texto"></textarea>
-                <textarea value={inputTwo} onChange={e => setInputTwo(e.target.value)} placeholder="Ingresa el segundo texto"></textarea>
-            </div>
-            <button onClick={compareTexts}>Comparar</button>
-            <div className={styles.result}>{difference}</div>
-        </div>
-    );
+  return (
+    <div className={styles.comparator}>
+      <div className={styles.boxContainer}>
+        <div 
+          contentEditable 
+          className={styles.textArea} 
+          onInput={(e) => setInputOne(e.currentTarget.textContent)}
+          dangerouslySetInnerHTML={{ __html: highlightedTextOne }}
+        />
+        <div 
+          contentEditable 
+          className={styles.textArea} 
+          onInput={(e) => setInputTwo(e.currentTarget.textContent)}
+          dangerouslySetInnerHTML={{ __html: highlightedTextTwo }}
+        />
+      </div>
+      <button onClick={() => compareTexts(inputOne, inputTwo)}>Comparar</button>
+    </div>
+  );
 };
 
 export default Comparator;
--- src/utils/compareUtils.ts
+++ src/utils/compareUtils.ts
@@ -1,5 +1,17 @@
 export const compareTexts = (text1: string, text2: string) => {
-    // Implementa tu lógica de comparación aquí
-    // Por ejemplo, una comparación simple podría ser:
-    return text1 === text2 ? "Los textos son iguales" : "Los textos son diferentes";
+    let resultOne = "";
+    let resultTwo = "";
+
+    // Simplemente resalta las diferencias entre dos textos
+    for (let i = 0; i < Math.max(text1.length, text2.length); i++) {
+        if (text1[i] !== text2[i]) {
+            resultOne += `<span class='highlight'>${text1[i] ?? ''}</span>`;
+            resultTwo += `<span class='highlight'>${text2[i] ?? ''}</span>`;
+        } else {
+            resultOne += text1[i] ?? '';
+            resultTwo += text2[i] ?? '';
+        }
+    }
+
+    return { textOne: resultOne, textTwo: resultTwo };
 };
