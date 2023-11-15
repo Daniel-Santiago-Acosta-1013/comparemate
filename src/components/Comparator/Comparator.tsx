791a368f01d55205dfcca1e7a604bce659b1c636
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-11-15 11:12:30 -0500

    [add][fix] improving comparator design

--- src/components/Comparator/Comparator.tsx
+++ src/components/Comparator/Comparator.tsx
@@ -2,23 +2,25 @@ import React, { useState } from 'react';
 import styles from './Comparator.module.scss';
 
 const Comparator = () => {
-  const [inputOne, setInputOne] = useState('');
-  const [inputTwo, setInputTwo] = useState('');
-  const [difference, setDifference] = useState('');
+    const [inputOne, setInputOne] = useState('');
+    const [inputTwo, setInputTwo] = useState('');
+    const [difference, setDifference] = useState('');
 
-  const compareTexts = () => {
-    // Lógica de comparación aquí (CompareUtils)
-    setDifference('Resultado de la comparación');
-  };
+    const compareTexts = () => {
+        // Lógica de comparación aquí (CompareUtils)
+        setDifference('Resultado de la comparación');
+    };
 
-  return (
-    <div className={styles.comparator}>
-      <textarea value={inputOne} onChange={e => setInputOne(e.target.value)} placeholder="Ingresa el primer texto"></textarea>
-      <textarea value={inputTwo} onChange={e => setInputTwo(e.target.value)} placeholder="Ingresa el segundo texto"></textarea>
-      <button onClick={compareTexts}>Comparar</button>
-      <div className={styles.result}>{difference}</div>
-    </div>
-  );
+    return (
+        <div className={styles.comparator}>
+            <div className={styles.boxContainer}>
+                <textarea value={inputOne} onChange={e => setInputOne(e.target.value)} placeholder="Ingresa el primer texto"></textarea>
+                <textarea value={inputTwo} onChange={e => setInputTwo(e.target.value)} placeholder="Ingresa el segundo texto"></textarea>
+            </div>
+            <button onClick={compareTexts}>Comparar</button>
+            <div className={styles.result}>{difference}</div>
+        </div>
+    );
 };
 
 export default Comparator;
