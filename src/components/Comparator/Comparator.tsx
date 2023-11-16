dc7ddbb557c0515e4b70322d37f5f4671f07de37
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-11-15 19:51:31 -0500

    [fix][add] implementing monaco to improve diff editor issues

--- src/components/Comparator/Comparator.tsx
+++ src/components/Comparator/Comparator.tsx
@@ -1,36 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import MonacoEditor, { useMonaco } from '@monaco-editor/react';
 import styles from './Comparator.module.scss';
-import { compareTexts } from '../../utils/compareUtils';
 
 const Comparator = () => {
   const [inputOne, setInputOne] = useState('');
   const [inputTwo, setInputTwo] = useState('');
-  const [highlightedTextOne, setHighlightedTextOne] = useState('');
-  const [highlightedTextTwo, setHighlightedTextTwo] = useState('');
-
+  const [language, setLanguage] = useState('javascript');
+  const monaco = useMonaco();
+  const editorRef = useRef(null);
+  
   useEffect(() => {
-    const { textOne, textTwo } = compareTexts(inputOne, inputTwo);
-    setHighlightedTextOne(textOne);
-    setHighlightedTextTwo(textTwo);
-  }, [inputOne, inputTwo]);
+    if (monaco && editorRef.current) {
+      const originalModel = monaco.editor.createModel(inputOne, language);
+      const modifiedModel = monaco.editor.createModel(inputTwo, language);
+
+      const diffEditor = monaco.editor.createDiffEditor(editorRef.current, {
+        readOnly: false,
+        automaticLayout: true,
+        theme: 'vs-dark', // Modo oscuro
+        enableSplitViewResizing: true,
+        renderSideBySide: true
+      });
+
+      diffEditor.setModel({
+        original: originalModel,
+        modified: modifiedModel
+      });
+
+      // Funcionalidad para mover diferencias de un lado a otro
+      diffEditor.getModifiedEditor().addAction({
+        id: 'my-unique-id',
+        label: 'Move Change Left',
+        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S],
+        contextMenuGroupId: '9_cutcopypaste',
+        contextMenuOrder: 1,
+        run: function (editor) {
+          // Implementar lógica para mover el cambio
+        }
+      });
+
+      return () => diffEditor.dispose();
+    }
+  }, [monaco, inputOne, inputTwo, language]);
 
   return (
     <div className={styles.comparator}>
+      <select onChange={(e) => setLanguage(e.target.value)} value={language}>
+        <option value="javascript">JavaScript</option>
+        <option value="python">Python</option>
+        <option value="java">Java</option>
+      </select>
       <div className={styles.boxContainer}>
-        <div 
-          contentEditable 
-          className={styles.textArea} 
-          onInput={(e) => setInputOne(e.currentTarget.textContent)}
-          dangerouslySetInnerHTML={{ __html: highlightedTextOne }}
+        <MonacoEditor
+          height="400px"
+          language={language}
+          value={inputOne}
+          onChange={setInputOne}
+          options={{ lineNumbers: 'on' }}
         />
-        <div 
-          contentEditable 
-          className={styles.textArea} 
-          onInput={(e) => setInputTwo(e.currentTarget.textContent)}
-          dangerouslySetInnerHTML={{ __html: highlightedTextTwo }}
+        <MonacoEditor
+          height="400px"
+          language={language}
+          value={inputTwo}
+          onChange={setInputTwo}
+          options={{ lineNumbers: 'on' }}
         />
       </div>
-      <button onClick={() => compareTexts(inputOne, inputTwo)}>Comparar</button>
+      
+      <div ref={editorRef} className={styles.diffEditor} style={{ height: '400px', width: '100%' }} />
+
+      
     </div>
   );
 };
