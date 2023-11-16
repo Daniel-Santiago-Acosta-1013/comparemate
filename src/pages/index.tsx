4b88a594fe8fba4a3dc381383382ee60068dd512
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-11-15 20:22:17 -0500

    [fix][add] adding head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
 import Comparator from '../components/Comparator/Comparator';
 import styles from '../styles/Home.module.scss'
 
 const HomePage = () => {
   return (
-    <div className={styles.home}>
-      <h1>Bienvenido a CompareMate</h1>
-      <Comparator />
-    </div>
+    <>
+      <Head>
+        <title>CompareMate</title>
+        <link rel="icon" href="/next.svg" />
+      </Head>
+      <div className={styles.home}>
+        <h1>Bienvenido a CompareMate</h1>
+        <Comparator />
+      </div>
+    </>
   );
 };
 
